31e99de0084976b4e37c7df35649fdddd81f7bdd
Shehzad Aslam Oza <32712842+ShehzadAslamOza@users.noreply.github.com>
2019-12-01 12:57:41 +0500

    Some algorithms added

---
+++ Algorithms/LinearSearch.py
@@ -0,0 +1,28 @@
+# Linear Search is basically looking into every element of the array and seeing if the required element is
+# present or not
+
+names = ["shehzad","kashan","ahmar","ghufran","maulana sahab"]
+found = False
+length = len(names)
+index = 0
+
+name = input("Enter your name: ")
+
+while not found and index < length - 1:
+    if names[index] == name:
+        found = True
+    else:
+        index += 1
+
+if found:
+    print("You are registered")
+else:
+    print("Proceed to counter number 1 for more details. ")
+
+# This code is done through in function // Alternate code
+
+#name = input("Enter your name: ")
+#if name in names:
+#    print("You are already registered. ")
+#else:
+#    print("Proceed to counter number 1 for more details. ")
---
+++ Algorithms/Populating3Darray.py
@@ -0,0 +1,26 @@
+import pprint
+
+# Driver code
+l = 4
+w = 3
+h = 2
+
+# Setting Dimenstions of a python #D array
+def list_dim(l,w,h):
+    my_array = [[['a' for m in range(l)] for i in range(w)] for k in range(h)]  # read list comprehensions
+    return my_array  # pprint basically displays list in proper format
+
+array_3d  = list_dim(l,w,h).copy()
+
+# populating array
+i = 0
+m = 0
+k = 0
+for i in range(h):
+    array_3d[i][m][k] = i
+    for m in range(w):
+        array_3d[i][m][k] = m
+        for k in range(l):
+            array_3d[i][m][k] = k
+
+pprint.pprint(array_3d)  # displays array in proper format
